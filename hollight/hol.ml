0dcdb9c473021030752f315da179c4ecdc39b69b
Phil Scott <phil.scott@ed.ac.uk>
2017-02-09 10:10:29 +0000

    Load serialise.ml.

--- hollight/hol.ml
+++ hollight/hol.ml
@@ -275,4 +275,6 @@ loads "define.ml";;     (* Support for general recursive definitions         *)
 loads "help.ml";;       (* Online help using the entries in Help directory   *)
 loads "database.ml";;   (* List of name-theorem pairs for search system      *)
 
+loadt "serialise.ml";;
+
 open Pervasives;;
