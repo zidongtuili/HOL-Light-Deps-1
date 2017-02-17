b4417e186b53229965e298c6f7aeaa6aafe013e2
Phil Scott <me@philscotted.com>
2017-02-17 07:01:22 +0000

    Aesthetic fix.
    Don't put back quantifiers if the variable is not free in the conjunct (unless the
    variable didn't occur in the conjunction of the matrix to begin with).

--- hollight/meta_conj.ml
+++ hollight/meta_conj.ml
@@ -3,16 +3,19 @@
 let split_and_reprove thm =
   let (asl,c) = dest_thm thm in
   let vs,thm' = splitlist SPEC_VAR thm in
-  let conjs = map (itlist GEN vs) (CONJUNCTS thm') in
+  let conjs = CONJUNCTS thm' in
+  let keep_v thm v = not (free_in v (concl thm')) || free_in v (concl thm) in
+  let vss = map (fun c -> filter (keep_v c) vs) conjs in
+  let conjs = map2 (itlist GEN) vss (CONJUNCTS thm') in
   match conjs with
   | [_] -> let id,thm,is_new = with_tracking_nodup thm in
            thm,[id,thm,is_new]
   | _::_ ->
      let conjs = map (with_tracking_nodup) conjs in
      let snd (_,x,_) = x in
-     let trivial = map (ACCEPT_TAC o SPECL vs o snd) conjs in
-     clear_local (TAC_PROOF ((map (fun asm -> ("",ASSUME asm)) asl, c),
-                             REPEAT GEN_TAC THEN REPEAT CONJ_TAC THENL trivial)),
+     let trivial = map2 (fun vs -> ACCEPT_TAC o SPECL vs o snd) vss conjs in
+     TAC_PROOF ((map (fun asm -> ("",ASSUME asm)) asl, c),
+                REPEAT GEN_TAC THEN REPEAT CONJ_TAC THENL trivial),
      conjs
 
 let zip_with_index xs =
